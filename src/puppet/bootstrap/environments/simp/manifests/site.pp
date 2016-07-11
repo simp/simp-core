30c2e9f6b74ebf6de1620ccc91ceace680534526
Nick Miller <nick.miller@onyxpoint.com>
2016-07-11 12:27:33 -0400

    (SIMP-1110) Document ::hostgroup usage in Hiera
    Provide an example on how to use the `::hostgroup` variable in site.pp
    
    SIMP-1110 #comment Updated 4.2.X

--- src/puppet/bootstrap/environments/simp/manifests/site.pp
+++ src/puppet/bootstrap/environments/simp/manifests/site.pp
@@ -25,4 +25,15 @@ Exec {
 # Added to map simp to NIST 800-53 Rev4
 $compliance_profile = 'nist_800_53_rev4'
 
+# Place Hiera customizations based on this variable in hieradata/hostgroups/${::hostgroup}.yaml
+#
+# Example hostgroup delcaration using a regex match on the hostname:
+#   if $::fqdn =~ /ws\d+\.${::domain}/ {
+#     $hostgroup = 'workstations'
+#   }
+#   else {
+#     $hostgroup = 'default'
+#   }
+$hostgroup = 'default'
+
 hiera_include('classes')
