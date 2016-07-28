1af1f560a223cba9963dde0c08ef22094f0c8505
Trevor Vaughan <tvaughan@onyxpoint.com>
2016-07-27 23:11:33 -0400

    (SIMP-1165) Optimize the build test
    Optimized the docker nodesets and build structure
    
    SIMP-1165 #comment Build Test Optimization
    
    Change-Id: Ib923dfffaba6397d2918d3ae9d058df7255ae447

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -28,16 +28,15 @@ RSpec.configure do |c|
     begin
       # Install modules and dependencies from spec/fixtures/modules
       copy_fixture_modules_to( hosts )
-      server = only_host_with_role(hosts, 'server')
 
       # Generate and install PKI certificates on each SUT
       Dir.mktmpdir do |cert_dir|
-        run_fake_pki_ca_on(server, hosts, cert_dir )
-        hosts.each{ |sut| copy_pki_to( sut, cert_dir, '/etc/pki/simp-testing' )}
+        run_fake_pki_ca_on(default, hosts, cert_dir )
+        hosts.each{ |sut| copy_pki_to(sut, cert_dir, '/etc/pki/simp-testing' )}
       end
 
       # add PKI keys
-      copy_keydist_to(server)
+      copy_keydist_to(default)
     rescue StandardError, ScriptError => e
       if ENV['PRY']
         require 'pry'; binding.pry
