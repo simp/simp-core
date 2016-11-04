2ff6f42dd9e57778757ef24a83441810ad917f19
Trevor Vaughan <tvaughan@onyxpoint.com>
2016-11-04 16:30:47 -0400

    (SIMP-1850) Updates for the 6.X build
    * Updated the test suites
    * Updated the package list
    * Updated the Gem releases
    
    SIMP-1850 #comment Start of the 6.0.0-Alpha release
    
    Change-Id: I7f8e684186650968ec5fdab3dc47a3e4b4cec7a8

--- spec/acceptance/suites/default/00_iso_build_spec.rb
+++ spec/acceptance/suites/default/00_iso_build_spec.rb
@@ -1,3 +1,8 @@
+# You can set the following environment variables to affect this test
+#
+# * SIMP_BEAKER_build_version => The git branch that you want to use
+# * SIMP_BEAKER_build_map     => The release_mapper.yaml key that you want to use
+#
 require 'spec_helper_acceptance'
 
 test_name 'iso build'
@@ -16,6 +21,14 @@ if Dir.glob(File.join(sys_iso_dir, '*.iso')).empty?
   fail(%(Error: The ISO directory '#{sys_iso_dir}' contains no ISO images))
 end
 
+# Custom Gemfile Support
+gemfile = nil
+gemfile_path = File.expand_path(File.join(fixtures_path,'Gemfile'))
+
+if File.file?(gemfile_path)
+  gemfile = gemfile_path
+end
+
 describe 'iso build' do
   context 'build SIMP ISO' do
 
@@ -30,16 +43,28 @@ describe 'iso build' do
     }
 
     # The branches inside of simp-core that should be built
+    #
+    # The hash should be of the form:
+    # {
+    #   'BRANCH' => 'Build Target from release_mappings.yaml'
+    # }
     let(:build_branches) {
-      to_build = []
+      to_build = {}
 
       if ENV['SIMP_BEAKER_build_version']
-        to_build << ENV['SIMP_BEAKER_build_version']
+        to_build[ENV['SIMP_BEAKER_build_version']] = ''
+
+        if ENV['SIMP_BEAKER_build_map']
+          to_build[ENV['SIMP_BEAKER_build_version']] = ENV['SIMP_BEAKER_build_map']
+        else
+          to_build[ENV['SIMP_BEAKER_build_version']] = ENV['SIMP_BEAKER_build_version']
+        end
       else
-        to_build = [
-          '5.1.X',
-          '4.2.X'
-        ]
+        to_build = {
+          '5.1.X' => '5.1.X',
+          '4.2.X' => '4.2.X',
+          'DentAuthurDent' => '5.1.X'
+        }
       end
 
       to_build
@@ -48,6 +73,12 @@ describe 'iso build' do
     hosts.each do |host|
 
       unless host[:hypervisor] == 'docker'
+        it 'should enable haveged on the system' do
+          host.install_package('haveged')
+
+          on(host, 'puppet resource service haveged ensure=running enable=true')
+        end
+
         it 'should add the build user to the system' do
           on(host, %(useradd -b /home -m -c 'Build User' -s /bin/bash -U #{build_user}))
           on(host, %(echo 'Defaults:#{build_user} !requiretty' >> /etc/sudoers))
@@ -73,7 +104,8 @@ describe 'iso build' do
             'gcc-c++',
             'ruby-devel',
             'rpm-build',
-            'rpm-devel'
+            'rpm-devel',
+            'rpm-sign'
           ]
 
           os = fact_on(host, 'operatingsystem')
@@ -120,8 +152,17 @@ describe 'iso build' do
       end
 
       it 'should clone the main repos' do
-        build_branches.each do |branch|
+        build_branches.each_key do |branch|
           on(host, %(#{run_cmd} "git clone -b #{branch} #{source_repo} #{branch}"))
+        end
+      end
+
+      if gemfile
+        it 'should have a custom Gemfile' do
+          scp_to(host, gemfile, '/tmp/Gemfile')
+          build_branches.each_key do |branch|
+            on(host, %(#{run_cmd} "cp /tmp/Gemfile #{branch}"))
+          end
         end
       end
 
@@ -129,10 +170,10 @@ describe 'iso build' do
         require 'time'
         timestamp = Time.now.iso8601
 
-        build_branches.each do |branch|
+        build_branches.each_pair do |branch, release_version|
 
           on(host, %(#{run_cmd} "rvm use default; cd #{branch}; bundle update"))
-          on(host, %(#{run_cmd} "rvm use default; cd #{branch}; rake build:auto[#{branch},/ISO]"))
+          on(host, %(#{run_cmd} "rvm use default; cd #{branch}; rake build:auto[#{release_version},/ISO]"))
 
           target_dir = File.expand_path("SIMP_ISO/rake_generated/#{branch}/#{timestamp}")
           FileUtils.mkdir_p(target_dir) unless File.directory?(target_dir)
