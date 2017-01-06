bc12a4fe5394b549da4ff489e385c3bbbc366c7d
Nick Miller <nick.miller@onyxpoint.com>
2017-01-06 14:01:25 -0500

    (SIMP-2240) Introduce SIMP Levels
    This commit adds different default class lists, depending on the number
    of SIMP features a user might want to use.

--- src/assets/simp-environment/environments/simp/manifests/site.pp
+++ src/assets/simp-environment/environments/simp/manifests/site.pp
@@ -11,13 +11,23 @@ Exec {
 }
 
 
+# SIMP level
+#
+# Set this variable to make use of the different class sets in heiradata/simp:
+#   * `simp` - compliant and secure
+#   * `simp-lite` - makes use of many of our modules, but doesn't apply
+#        any prohibitive security or compliance features
+#   * `poss` or any other setting - don't include any classes by default
+$simp_level = 'simp'
+
 # Map SIMP parameters to NIST Special Publication 800-53, Revision 4
+# See hieradata/compliance_profiles/ for more options.
 $compliance_profile = 'nist_800_53_rev4'
 
 # Place Hiera customizations based on this variable in hieradata/hostgroups/${::hostgroup}.yaml
 #
 # Example hostgroup declaration using a regex match on the hostname:
-#   if $::fqdn =~ /ws\d+\.<domain>/ {
+#   if $facts['fqdn'] =~ /ws\d+\.<domain>/ {
 #     $hostgroup = 'workstations'
 #   }
 #   else {
@@ -26,7 +36,9 @@ $compliance_profile = 'nist_800_53_rev4'
 #
 $hostgroup = 'default'
 
-# Hiera class lookups and inclusions (replaces `hiera_include()`)
+# Add Puppet classes to the `classes` array in hiera to add them to the system.
+# For special cases where a class needs to be removed from the classes array, you
+# can use the `class_exclusions` array and it will be subtracted.
 $hiera_classes          = lookup('classes',          Array[String], 'unique', [])
 $hiera_class_exclusions = lookup('class_exclusions', Array[String], 'unique', [])
 $hiera_included_classes = $hiera_classes - $hiera_class_exclusions
