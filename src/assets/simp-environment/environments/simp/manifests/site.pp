59baf9421375bbd66d244d8d0855c73088c32225
Nick Miller <nick.miller@onyxpoint.com>
2017-01-17 14:12:52 -0500

    Fixed trailing newlines

--- src/assets/simp-environment/environments/simp/manifests/site.pp
+++ src/assets/simp-environment/environments/simp/manifests/site.pp
@@ -46,3 +46,4 @@ $hiera_classes          = lookup('classes',          Array[String], 'unique', []
 $hiera_class_exclusions = lookup('class_exclusions', Array[String], 'unique', [])
 $hiera_included_classes = $hiera_classes - $hiera_class_exclusions
 include $hiera_included_classes
+
