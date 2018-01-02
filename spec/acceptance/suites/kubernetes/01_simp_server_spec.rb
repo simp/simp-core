52d14e1d3d792b6c00f1d8f6cfa928ed21cc0599
Nick Miller <nick.miller@onyxpoint.com>
2018-01-02 16:19:00 -0500

    (SIMP-4258) k8s: Add kubernetes test (#488)
    * test fix for new beaker helpers

--- spec/acceptance/suites/kubernetes/01_simp_server_spec.rb
+++ spec/acceptance/suites/kubernetes/01_simp_server_spec.rb
@@ -1,5 +1,6 @@
 require 'spec_helper_integration'
 require 'beaker/puppet_install_helper'
+require 'yaml'
 
 # Create a Puppetfile for R10K from module
 # portion of a simp-core Puppetfile.<tracking|stable>.
@@ -133,66 +134,19 @@ describe 'install environment via r10k and puppetserver' do
         include 'pupmod::master'
       }
     EOF
-    default_yaml = <<-EOF
-      # Options
-      simp_options::dns::servers: ['8.8.8.8']
-      simp_options::puppet::server: #{master_fqdn}
-      simp_options::puppet::ca: #{master_fqdn}
-      simp_options::ntpd::servers: ['time.nist.gov']
-      simp_options::ldap::bind_pw: 's00per sekr3t!'
-      simp_options::ldap::bind_hash: '{SSHA}foobarbaz!!!!'
-      simp_options::ldap::sync_pw: 's00per sekr3t!'
-      simp_options::ldap::sync_hash: '{SSHA}foobarbaz!!!!'
-      simp_options::ldap::root_hash: '{SSHA}foobarbaz!!!!'
-      simp_options::auditd: true
-      simp_options::fips: false
-      simp_options::haveged: true
-      simp_options::logrotate: true
-      simp_options::pam: true
-      simp_options::selinux: true
-      simp_options::stunnel: true
-      simp_options::tcpwrappers: true
-      simp_options::firewall: true
 
-      # simp_options::log_servers: ['#{master_fqdn}']
-      sssd::domains: ['LOCAL']
-      simp::yum::servers: ['#{master_fqdn}']
+    default_hiera = YAML.load_file('spec/acceptance/suites/kubernetes/files/hieradata.yaml').merge({
+        'simp_options::puppet::server' => master_fqdn,
+        'simp_options::puppet::ca'     => master_fqdn,
+        'simp::yum::servers'           => [master_fqdn],
+      }
+    )
 
-      # Settings required for acceptance test, some may be required
-      simp::scenario: simp
-      simp_options::clamav: false
-      simp_options::pki: true
-      simp_options::pki::source: '/etc/pki/simp-testing/pki'
-      simp_options::trusted_nets: ['ALL']
-      simp::yum::os_update_url: http://mirror.centos.org/centos/$releasever/os/$basearch/
-      simp::yum::enable_simp_repos: false
-      simp::scenario::base::puppet_server_hosts_entry: false
-      # Make sure puppet doesn't run (hopefully)
-      pupmod::agent::cron::minute: '0'
-      pupmod::agent::cron::hour: '0'
-      pupmod::agent::cron::weekday: '0'
-      pupmod::agent::cron::month: '1'
-
-      # Settings to make beaker happy
-      sudo::user_specifications:
-        vagrant_all:
-          user_list: ['vagrant']
-          cmnd: ['ALL']
-          passwd: false
-      pam::access::users:
-        defaults:
-          origins:
-            - ALL
-          permission: '+'
-        vagrant:
-      ssh::server::conf::permitrootlogin: true
-      ssh::server::conf::authorizedkeysfile: .ssh/authorized_keys
-    EOF
     it 'should install the control repo' do
       on(master, 'mkdir -p /etc/puppetlabs/code/environments/production/{hieradata,manifests} /var/simp/environments/production/{simp_autofiles,site_files/modules/pki_files/files/keydist}')
       scp_to(master, 'spec/acceptance/suites/default/files/hiera.yaml', '/etc/puppetlabs/puppet/hiera.yaml')
       create_remote_file(master, '/etc/puppetlabs/code/environments/production/manifests/site.pp', site_pp)
-      create_remote_file(master, '/etc/puppetlabs/code/environments/production/hieradata/default.yaml', default_yaml)
+      create_remote_file(master, '/etc/puppetlabs/code/environments/production/hieradata/default.yaml', default_hiera.to_yaml)
       on(master, 'chown -R root.puppet /etc/puppetlabs/code/environments/production/{hieradata,manifests} /var/simp/environments/production/site_files/modules/pki_files/files/keydist')
       on(master, 'chmod -R g+rX /etc/puppetlabs/code/environments/production/{hieradata,manifests} /var/simp/environments/production/site_files/modules/pki_files/files/keydist')
       on(master, 'chown -R puppet.puppet /var/simp/environments/production/simp_autofiles')
@@ -204,16 +158,25 @@ describe 'install environment via r10k and puppetserver' do
     agents.each do |agent|
       it 'should run the agent' do
         # require 'pry';binding.pry if fact_on(agent, 'hostname') == 'agent'
-        on(agent, "puppet agent -t --ca_port 8141 --server #{master_fqdn}", :acceptable_exit_codes => [0,2,4,6])
+        on(agent, "puppet agent -t --ca_port 8141 --server #{master_fqdn}",
+          acceptable_exit_codes: [0,2,4,6],
+          run_in_parallel: true
+        )
         Simp::TestHelpers.wait(30)
-        on(agent, "puppet agent -t --ca_port 8141 --server #{master_fqdn}", :acceptable_exit_codes => [0,2,4,6])
+        on(agent, "puppet agent -t --ca_port 8141 --server #{master_fqdn}",
+          acceptable_exit_codes: [0,2,4,6],
+          run_in_parallel: true )
         agent.reboot
         Simp::TestHelpers.wait(240)
-        on(agent, "puppet agent -t --ca_port 8141 --server #{master_fqdn}", :acceptable_exit_codes => [0,2])
+        on(agent, "puppet agent -t --ca_port 8141 --server #{master_fqdn}",
+          acceptable_exit_codes: [0,2],
+          run_in_parallel: true )
       end
       it 'should be idempotent' do
         Simp::TestHelpers.wait(30)
-        on(agent, 'puppet agent -t', :acceptable_exit_codes => [0])
+        on(agent, 'puppet agent -t',
+          acceptable_exit_codes: [0],
+          run_in_parallel: true )
       end
     end
   end
