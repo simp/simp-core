76607026501a8a8a35cb43403b85c0d923af7d57
Trevor Vaughan <tvaughan@onyxpoint.com>
2018-07-09 11:35:25 -0400

    (SIMP-4989) Update Puppetfile.tracking for Beta 2
    * Updated the Puppetfile to hold corrected version information for
      6.2.0-Beta2
    * Updatd the simp.spec file to cover added, removed, and modified files
      and version numbers
    
    TODO:
    
      * Get beaker:suites running properly (having parallel issues)
      * Fix package downloads
    
    SIMP-4990 #close

--- spec/acceptance/suites/default/03_simp_server_ldap_spec.rb
+++ spec/acceptance/suites/default/03_simp_server_ldap_spec.rb
@@ -41,7 +41,7 @@ describe 'use the simp::server::ldap class to create and ldap environment' do
 
   context 'agents' do
     it 'set up and run puppet' do
-      block_on(agents, :run_in_parallel => true) do |agent|
+      block_on(agents, :run_in_parallel => false) do |agent|
         retry_on(agent, 'puppet agent -t',
           :desired_exit_codes => [0],
           :retry_interval     => 15,
