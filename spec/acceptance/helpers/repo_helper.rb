e05f2baa8be4729e75b5f81b92ac32d9e4a57c57
Trevor Vaughan <tvaughan@onyxpoint.com>
2020-10-01 09:03:03 -0400

    (SIMP-8386) Removed packagecloud from the tests
    * Changed:
      * Require simp-beaker-helpers 1.19+
      * Removed packagecloud from the EL7 simp.repo definition
      * Migrated to simp-beaker-helpers management of the SIMP repos
      * Added the EL6 SSH ciphers workaround to the nodesets
    
    SIMP-8386 #close

--- spec/acceptance/helpers/repo_helper.rb
+++ spec/acceptance/helpers/repo_helper.rb
@@ -50,71 +50,10 @@ module Acceptance
         end
       end
 
-      # Install a SIMP packagecloud yum repo
-      #
-      # - Each repo is modeled after what appears in simp-doc
-      # - See https://packagecloud.io/simp-project/ for the reponame key
-      #
-      # +host+: Host object on which SIMP repo(s) will be installed
-      # +reponame+: The base name of the repo, e.g. '6_X'
-      # +type+: Which repo to install:
-      #   :main for the main repo containing SIMP puppet modules
-      #   :deps for the SIMP dependency repo containing OS or application
-      #         RPMs not available from standard CentOS repos
-      #
-      # @fails if the specified repo cannot be installed on host
-      def install_internet_simp_repo(host, reponame, type)
-        case type
-        when :main
-          full_reponame = reponame
-          # FIXME: Use a gpgkey list appropriate for more than 6_X
-          repo = <<~EOM
-            [simp-project_#{reponame}]
-            name=simp-project_#{reponame}
-            baseurl=https://packagecloud.io/simp-project/#{reponame}/el/$releasever/$basearch
-            gpgcheck=1
-            enabled=1
-            gpgkey=https://raw.githubusercontent.com/NationalSecurityAgency/SIMP/master/GPGKEYS/RPM-GPG-KEY-SIMP
-                   https://download.simp-project.com/simp/GPGKEYS/RPM-GPG-KEY-SIMP-6
-            sslverify=1
-            sslcacert=/etc/pki/tls/certs/ca-bundle.crt
-            metadata_expire=300
-          EOM
-        when :deps
-          full_reponame = "#{reponame}_Dependencies"
-          # FIXME: Use a gpgkey list appropriate for more than 6_X
-          repo = <<~EOM
-            [simp-project_#{reponame}_dependencies]
-            name=simp-project_#{reponame}_dependencies
-            baseurl=https://packagecloud.io/simp-project/#{reponame}_Dependencies/el/$releasever/$basearch
-            gpgcheck=1
-            enabled=1
-            gpgkey=https://raw.githubusercontent.com/NationalSecurityAgency/SIMP/master/GPGKEYS/RPM-GPG-KEY-SIMP
-                   https://download.simp-project.com/simp/GPGKEYS/RPM-GPG-KEY-SIMP-6
-                   https://yum.puppet.com/RPM-GPG-KEY-puppetlabs
-                   https://yum.puppet.com/RPM-GPG-KEY-puppet
-                   https://apt.postgresql.org/pub/repos/yum/RPM-GPG-KEY-PGDG-96
-                   https://dl.fedoraproject.org/pub/epel/RPM-GPG-KEY-EPEL-$releasever
-            sslverify=1
-            sslcacert=/etc/pki/tls/certs/ca-bundle.crt
-            metadata_expire=300
-          EOM
-          full_reponame = "#{reponame}_Dependencies"
-        else
-          fail("install_internet_simp_repo() Unknown repo type specified '#{type.to_s}'")
-        end
-        puts('='*72)
-        puts("Using SIMP #{full_reponame} Internet repo from packagecloud")
-        puts('='*72)
-
-        create_remote_file(host, "/etc/yum.repos.d/simp-project_#{full_reponame.downcase}.repo", repo)
-      end
-
       # Set up SIMP repos on the host
       #
-      # By default, the SIMP '6_X' repos available from packagecloud
-      # will be configured.  This can be overidden with the BEAKER_repo
-      # environment variable as follows:
+      # By default, the SIMP '6_X' repos will be configured.  This can be
+      # overidden with the BEAKER_repo environment variable as follows:
       # - When set to a fully qualified path of a repo file, the file will
       #   be installed as a repo on the host.  In this case set_up_simp_main
       #   and set_up_simp_deps are both ignored, as the repo file is assumed
@@ -133,8 +72,18 @@ module Acceptance
         if reponame[0] == '/'
           copy_repo(host, reponame)
         else
-          install_internet_simp_repo(host, reponame, :main) if set_up_simp_main
-          install_internet_simp_repo(host, reponame, :deps) if set_up_simp_deps
+          disable_list = []
+          unless set_up_simp_main
+            disable_list << 'simp-community-simp'
+          end
+
+          unless set_up_simp_deps
+            disable_list << 'simp-community-epel'
+            disable_list << 'simp-community-puppet'
+            disable_list << 'simp-community-postgresql'
+          end
+
+          install_simp_repos(host, disable_list)
         end
       end
 
