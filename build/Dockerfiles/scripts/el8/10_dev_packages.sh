c6356aee1d458b8290f08fdc446a071d01f78d4b
Trevor Vaughan <tvaughan@onyxpoint.com>
2021-02-04 09:46:56 -0500

    fix EL8 package script

--- build/Dockerfiles/scripts/el8/10_dev_packages.sh
+++ build/Dockerfiles/scripts/el8/10_dev_packages.sh
@@ -5,8 +5,7 @@ yum install -y epel-release
 yum install -y rpm-build rpmdevtools ruby-devel rpm-devel rpm-sign
 yum install -y util-linux openssl augeas-libs createrepo genisoimage git gnupg2 libicu-devel libxml2 libxml2-devel libxslt libxslt-devel which ruby-devel
 yum -y install scl-utils python2-virtualenv python3-virtualenv fontconfig dejavu-sans-fonts dejavu-sans-mono-fonts dejavu-serif-fonts dejavu-fonts-common libjpeg-devel zlib-devel openssl-devel
-yum install -y libyaml glibc-headers autoconf gcc gcc-c++ glibc-devel readline-devel libffi-devel automake libtool bison sqlite-devel
-ln -sf /bin/true /usr/bin/systemctl
+yum install -y libyaml glibc-headers autoconf gcc gcc-c++ glibc-devel readline-devel libffi-devel automake libtool bison sqlite-devel pinentry
 
 # Install helper packages
 yum install -y rubygems vim-enhanced jq
