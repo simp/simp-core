2704ed73c1e52a874cb7199766954f5c68a2f188
lnemsick-simp <lnemsick-simp@users.noreply.github.com>
2021-02-10 08:28:15 -0500

    fix typo in comment

--- build/Dockerfiles/scripts/el7/01_disable_systemctl.sh
+++ build/Dockerfiles/scripts/el7/01_disable_systemctl.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -e
 
-# The oldesst systemd doesn't work but new versions can skip this file
+# The oldest systemd doesn't work but new versions can skip this file
 ln -sf /bin/true /usr/bin/systemctl
