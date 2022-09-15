fbb1f50603b7ff176ef8c1887d16288caa9b6631
Trevor Vaughan <trevor@sicura.us>
2022-09-15 16:18:51 -0400

    remove obsolete code

--- spec/acceptance/helpers/system_gem_helper.rb
+++ spec/acceptance/helpers/system_gem_helper.rb
@@ -18,12 +18,7 @@ module Acceptance
 
         # beaker-helper fact_on() now uses '--json' on facter calls, so
         # we need to make sure the json gem is installed
-        result = on(host, 'facter --json fqdn', :accept_all_exit_codes => true)
-        return if result.exit_code.eql?(0)
-
-        # We have old system Ruby (1.8.7) which does not include json.  So,
-        # install the pure-Ruby version of json.
-        on(host, "gem install json_pure || gem install json_pure --version '<2.0.0'")
+        on(host, 'facter --json fqdn', :accept_all_exit_codes => true)
       end
 
       def uninstall_system_factor_gem(host)
